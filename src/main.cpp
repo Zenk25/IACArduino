04d540d5a8d77f7dce88dfe7a63f3d3088c12829
Zenk25 <Zenk25@users.noreply.github.com>
2019-04-10 14:34:06 +0100

    Cargado todo, dejando un 70% de la memoria flash libre.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,30 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SD.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network.
-// gateway and subnet are optional:
+
 byte mac[] = {
   0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
-};/*
-IPAddress ip(192, 168, 1, 177);
-IPAddress myDns(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 0, 0);
-*/
+};
 // telnet defaults to port 23
 EthernetServer server(80);
-boolean gotAMessage = false; // whether or not you got a message from the client yet
-
-
 
 void setup() {
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
+
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  //Iniciada la SDCard
+   if (!SD.begin(4)) {
+       return;    // init failed
+   }
+   // check for index.htm file
+   if (!SD.exists("index2.htm")) {
+       return;  // can't find index file
+   }
+
   // You can use Ethernet.init(pin) to configure the CS pin
   //Ethernet.init(10);  // Most Arduino shields
   //Ethernet.init(5);   // MKR ETH shield
@@ -30,25 +33,16 @@ void setup() {
   //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
   //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
 
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
   // start the Ethernet connection:
-  Serial.println("Trying to get an IP address using DHCP");
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");
     // Check for Ethernet hardware present
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+
       while (true) {
         delay(1); // do nothing, no point running without Ethernet hardware
       }
     }
     if (Ethernet.linkStatus() == LinkOFF) {
-      Serial.println("Ethernet cable is not connected.");
     }
     // initialize the Ethernet device not using DHCP:
    /* Ethernet.begin(mac, ip, myDns, gateway, subnet);*/
@@ -80,10 +74,17 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          //client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
-          String html ="<!DOCTYPE html> <html lang=\"es\" dir=\"ltr\"> <head> <meta charset=\"utf-8\"> <title>WebServer Arduino</title> </head> <body> <p>Hola Mundo</p> </body> </html>";
-          client.println(html);
+          //Enviar pagina WebServer
+          File webFile = SD.open("index2.htm");
+          if(webFile){
+            while(webFile.available()){
+              client.write(webFile.read());
+            }
+            webFile.close();
+          }
+          break;
         }
         if (c == '\n') {
           // you're starting a new line
@@ -98,6 +99,5 @@ void loop() {
     delay(1);
     // close the connection:
     client.stop();
-    Serial.println("client disconnected");
   }
 }
