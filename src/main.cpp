a8c7318c3f459cddb75bd94e4497d2dd28d82d64
Zenk25 <Zenk25@users.noreply.github.com>
2019-04-10 12:44:48 +0100

    Html creation tested and working.

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,44 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network.
+// gateway and subnet are optional:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-
-//Está IP depende de la red local que tenga cuando esté instalado el arduino. Preguntar a Héctor.
+  0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02
+};/*
 IPAddress ip(192, 168, 1, 177);
-//Puerto por defecto para HTTP.
+IPAddress myDns(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 0, 0);
+*/
+// telnet defaults to port 23
 EthernetServer server(80);
+boolean gotAMessage = false; // whether or not you got a message from the client yet
+
 
 
 void setup() {
-  // put your setup code here, to run once:
-  //Inicio del servidor HTTP, donde se cargará el HTML.
+  pinMode(4, OUTPUT);
+  digitalWrite(4, HIGH);
+  // You can use Ethernet.init(pin) to configure the CS pin
+  //Ethernet.init(10);  // Most Arduino shields
+  //Ethernet.init(5);   // MKR ETH shield
+  //Ethernet.init(0);   // Teensy 2.0
+  //Ethernet.init(20);  // Teensy++ 2.0
+  //Ethernet.init(15);  // ESP8266 with Adafruit Featherwing Ethernet
+  //Ethernet.init(33);  // ESP32 with Adafruit Featherwing Ethernet
+
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB port only
-    }
-    Serial.println("Ethernet WebServer Example");
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-    // start the Ethernet connection and the server:
-    Ethernet.begin(mac, ip);
-
+  // start the Ethernet connection:
+  Serial.println("Trying to get an IP address using DHCP");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
     // Check for Ethernet hardware present
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
       Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
@@ -34,17 +50,19 @@ void setup() {
     if (Ethernet.linkStatus() == LinkOFF) {
       Serial.println("Ethernet cable is not connected.");
     }
+    // initialize the Ethernet device not using DHCP:
+   /* Ethernet.begin(mac, ip, myDns, gateway, subnet);*/
+  }
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  Serial.println(Ethernet.localIP());
 
-    // start the server
-    server.begin();
-    Serial.print("server is at ");
-    Serial.println(Ethernet.localIP());
-  //Aquí se leerá el archivo de configuración
+  // start listening for clients
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Creación de la pagina web
+  // wait for a new client:
   EthernetClient client = server.available();
   if (client) {
     Serial.println("new client");
@@ -81,4 +99,5 @@ void loop() {
     // close the connection:
     client.stop();
     Serial.println("client disconnected");
+  }
 }
