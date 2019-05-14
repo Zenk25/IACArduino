5e1ef34727c7b6542d2f7804b1a5996e285325bc
Zenk25 <Zenk25@users.noreply.github.com>
2019-05-14 15:13:56 +0100

    Arreglando los delays, Probar usando el control en canal y luego monitor

--- lib/mediciones/Channel.cpp
+++ lib/mediciones/Channel.cpp
@@ -31,78 +31,81 @@ void Channel::setPort(int port2){
 }
 
 char* Channel::leer() {
-  switch(port){
-    case 1:
-    {
-      //Esta sección envía el código para medir el canal específicado.
-      Serial1.write("KRDG? ");
-      Serial1.print(numCanal);
-      Serial1.write("\r\n");
-      Serial1.println();
-      delay(2);
-      //Esta sección recibe el valor de la temperatura y la devuelve como un
-      //array de chars.
-      if(Serial1.available()){
-        int z= 0;
-        char lakeshore;
-        while((lakeshore = Serial1.read()) != '\r'){
-          valor[z] = lakeshore;
-          z++;
+  /*unsigned long oldtime = millis();
+  if((millis()-oldtime) > 500){*/
+    switch(port){
+      case 1:
+      {
+        //Esta sección envía el código para medir el canal específicado.
+        Serial1.write("KRDG? ");
+        Serial1.print(numCanal);
+        Serial1.write("\r\n");
+        Serial1.println();
+        delay(2);
+        //Esta sección recibe el valor de la temperatura y la devuelve como un
+        //array de chars.
+        if(Serial1.available()){
+          int z= 0;
+          char lakeshore;
+          while((lakeshore = Serial1.read()) != '\r'){
+            valor[z] = lakeshore;
+            z++;
+          }
+          return valor;
+        }else{
+          return nulo;
         }
-        return valor;
-      }else{
-        return nulo;
-      }
 
-    }
-    break;
-    case 2:
-    {
-      //Esta sección envía el código para medir el canal específicado.
-      Serial2.write("KRDG? ");
-      Serial2.print(numCanal);
-      Serial2.write("\r\n");
-      Serial2.println();
-      delay(2);
-      //Esta sección recibe el valor de la temperatura y la devuelve como un
-      //array de chars.
-      if(Serial2.available()){
-        int z= 0;
-        char lakeshore;
-        while((lakeshore = Serial2.read()) != '\r'){
-          valor[z] = lakeshore;
-          z++;
-        }
-        return valor;
-      }else{
-        return nulo;
       }
+      break;
+      case 2:
+      {
+        //Esta sección envía el código para medir el canal específicado.
+        Serial2.write("KRDG? ");
+        Serial2.print(numCanal);
+        Serial2.write("\r\n");
+        Serial2.println();
+        delay(2);
+        //Esta sección recibe el valor de la temperatura y la devuelve como un
+        //array de chars.
+        if(Serial2.available()){
+          int z= 0;
+          char lakeshore;
+          while((lakeshore = Serial2.read()) != '\r'){
+            valor[z] = lakeshore;
+            z++;
+          }
+          return valor;
+        }else{
+          return nulo;
+        }
 
-    }
-    break;
-    case 3:
-    {
-      //Esta sección envía el código para medir el canal específicado.
-      Serial3.write("KRDG? ");
-      Serial3.print(numCanal);
-      Serial3.write("\r\n");
-      Serial3.println();
-      delay(2);
-      //Esta sección recibe el valor de la temperatura y la devuelve como un
-      //array de chars.
-      if(Serial3.available()){
-        int z= 0;
-        char lakeshore;
-        while((lakeshore = Serial3.read()) != '\r'){
-          valor[z] = lakeshore;
-          z++;
-        }
-        return valor;
-      }else{
-        return nulo;
       }
+      break;
+      case 3:
+      {
+        //Esta sección envía el código para medir el canal específicado.
+        Serial3.write("KRDG? ");
+        Serial3.print(numCanal);
+        Serial3.write("\r\n");
+        Serial3.println();
+        delay(2);
+        //Esta sección recibe el valor de la temperatura y la devuelve como un
+        //array de chars.
+        if(Serial3.available()){
+          int z= 0;
+          char lakeshore;
+          while((lakeshore = Serial3.read()) != '\r'){
+            valor[z] = lakeshore;
+            z++;
+          }
+          return valor;
+        }else{
+          return nulo;
+        }
+      }
+      default:
+        return nulo;
     }
-    default:
-      return nulo;
-  }
+//  }
 };
