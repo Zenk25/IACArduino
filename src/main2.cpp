c30199ed941e22a2d670e7e3517a24cb80c4b105
Zenk25 <Zenk25@users.noreply.github.com>
2019-05-20 14:49:55 +0100

    Crear el upload.php, y cargarlo y estará terminado.

--- src/main2.cpp
+++ src/main2.cpp
@@ -10,7 +10,7 @@ byte mac[] = {
 
 unsigned int localPort = 8888;       // local port to listen for UDP packets
 
-const char timeServer[] = "1.europe.pool.ntp.org"; // time.nist.gov NTP server
+const char timeServer[] = "time.nist.gov"; // time.nist.gov NTP server
 
 const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
 
@@ -60,7 +60,7 @@ void sendNTPpacket(const char * address) {
   packetBuffer[1] = 0;     // Stratum, or type of clock
   packetBuffer[2] = 6;     // Polling Interval
   packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
+  // 8 bytes a cero para Root Delay & Root Dispersion
   packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
@@ -68,14 +68,22 @@ void sendNTPpacket(const char * address) {
 
   // all NTP fields have been given values, now
   // you can send a packet requesting a timestamp:
-  Udp.beginPacket(address, 123); // NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
+  if((Udp.beginPacket(address, 123))!=1){
+    delay(1000);
+    Serial.println(Udp.beginPacket(address,123));
+    //Si falla que no escriba la coma.
+  }
+  //los pedidos NTP se hacen a través del puerto 123
+
+  Serial.println(Udp.write(packetBuffer, NTP_PACKET_SIZE));
+
+  Serial.println(Udp.endPacket());
+
 }
 
 void loop() {
   sendNTPpacket(timeServer); // send an NTP packet to a time server
-
+  Serial.println("Enviando...");
   // wait to see if a reply is available
   delay(1000);
   if (Udp.parsePacket()) {
@@ -84,27 +92,16 @@ void loop() {
 
     // the timestamp starts at byte 40 of the received packet and is four bytes,
     // or two words, long. First, extract the two words:
-
+    Serial.write("Bien");
     unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
     unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
     // combine the four bytes (two words) into a long integer
     // this is NTP time (seconds since Jan 1 1900):
     unsigned long secsSince1900 = highWord << 16 | lowWord;
-    Serial.print("Seconds since Jan 1 1900 = ");
-    Serial.println(secsSince1900);
-
-    // now convert NTP time into everyday time:
-    Serial.print("Unix time = ");
     // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
     const unsigned long seventyYears = 2208988800UL;
     // subtract seventy years:
     unsigned long epoch = secsSince1900 - seventyYears;
-    // print Unix time:
-    Serial.println(epoch);
-
-
-    // print the hour, minute and second:
-    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
     Serial.print(((epoch+3600)  % 86400L) / 3600); // print the hour (86400 equals secs per day)
     Serial.print(':');
     if (((epoch % 3600) / 60) < 10) {
@@ -117,10 +114,11 @@ void loop() {
       // In the first 10 seconds of each minute, we'll want a leading '0'
       Serial.print('0');
     }
-    Serial.println(epoch % 60); // print the second
-  }
+    Serial.print(epoch % 60);// print the second
+    //Añadir fecha a la hora mostrada.
   // wait ten seconds before asking for the time again
-  delay(10000);
   Ethernet.maintain();
+  }
+  delay(10000);
 }
 */
